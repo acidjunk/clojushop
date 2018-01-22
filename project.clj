fddd7cdfa1a6f5a9fc1608d774168f75ab5c1f04
trickyBytes <richard.tiffin@akoolla.com>
2018-01-22 09:07:59 +0000

    Altered dependency versions to get the application to start. Still can't run the tests at the moment, but am working through more dependencies to see if I can them to pass too.

--- project.clj
+++ project.clj
@@ -3,15 +3,16 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
-                 [jayq "0.1.0-alpha3"]
+                 [jayq "0.1.0-alpha3"];"2.5.4"
                  [com.cemerick/friend "0.2.0"]
                  [ring/ring-json "0.3.0"]
-                 [ring-json-params "0.1.0"]
+
+                 [ring-json-params "0.1.3"];problem with
                  [cheshire "5.3.1"]
-                 [org.clojure/data.json "0.2.4"]
+                 [org.clojure/data.json "0.2.6"]
                  [com.novemberain/monger "1.7.0"]
                  [ring-middleware-format "0.3.2"]
-                 [org.clojure/core.cache "0.6.3"]
+                 [org.clojure/core.cache "0.6.5"]
                  [com.novemberain/validateur "2.1.0"]
                  [ring-basic-authentication "1.0.2"]
                  [abengoa/clj-stripe "1.0.4"]
@@ -22,4 +23,4 @@
   :ring {:handler clojushop.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring/ring-mock "0.3.0"]]}})
